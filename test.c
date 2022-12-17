ed60e2635594080476192551c960b40701188e57
BoxWorld <boxworld0088@gmail.com>
2022-12-17 13:43:05 +0800

    update stage-3

--- test.c
+++ test.c
@@ -1,14 +1,8 @@
-int f(int a[]){
-  a[0] = 10;
-  return a[1] + a[2] * 2 + a[3] * 3; 
-}
-
 int main(){
-  int b[4];
-  b[1] = 2;
-  b[2] = 3;
-  b[3] = 4;
-  int ans = f(b);
-  ans = ans + b[0] * 2;
-  return ans;
+  int a = 2;
+if (a < 3) {
+{
+int a = 3;
+return a; }
+return a; }
 }
